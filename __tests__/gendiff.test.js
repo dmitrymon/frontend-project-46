66c900a057d870910532cf2c28bccc1d4c9a859e
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-03-05 16:58:52 +0300

    add __tests__/gendiff.test.js

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,16 @@
+import path from 'path';
+import url from 'url';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures', filename);
+
+test('fileJSON', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const resultpath = getFixturePath('expectedJSON.txt');
+  const result = fs.readFileSync(resultpath, 'utf-8');
+  expect(genDiff(filepath1, filepath2)).toBe(result);
+});
