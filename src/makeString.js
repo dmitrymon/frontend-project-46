64512f4269cda300947f0e4a6a294b47393ffa3b
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-03-06 23:39:45 +0300

    fix src/makeString.js

--- src/makeString.js
+++ src/makeString.js
@@ -1,14 +1,14 @@
 const makeString = (object) => {
   const strings = object.map((element) => {
     if (element.type === 'added') {
-      return `+ ${element.key}: ${element.value}`;
+      return `  + ${element.key}: ${element.value}`;
     } if (element.type === 'deleted') {
-      return `- ${element.key}: ${element.value}`;
+      return `  - ${element.key}: ${element.value}`;
     } if (element.type === 'changed') {
-      return `- ${element.key}: ${element.value1}
-+ ${element.key}: ${element.value2}`;
+      return `  - ${element.key}: ${element.value1}
+  + ${element.key}: ${element.value2}`;
     }
-    return `  ${element.key}: ${element.value}`;
+    return `    ${element.key}: ${element.value}`;
   });
 
   const result = strings.join('\n');
