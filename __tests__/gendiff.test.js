7d62a977c3f782d42bc56fcb4cdb62c2353e0496
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-03-21 20:42:59 +0300

    change __tests__/gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,17 +8,19 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+const jsonFile1 = getFixturePath('file1.json');
+const jsonFile2 = getFixturePath('file2.json');
+
+const yamlFile1 = getFixturePath('file1.yaml');
+const yamlFile2 = getFixturePath('file2.yaml');
+
 const resultpath = getFixturePath('expected.txt');
 const result = fs.readFileSync(resultpath, 'utf-8');
 
-test('fileJSON', () => {
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
-  expect(genDiff(filepath1, filepath2)).toBe(result);
-});
+test.each([
+{ file1: jsonFile1, file2: jsonFile2, expected: result },
+{ file1: yamlFile1, file2: yamlFile2, expected: result },
+])('diff tests', (file1, file2, expected) => {
 
-test('fileYAML', () => {
-  const filepath1 = getFixturePath('file1.yaml');
-  const filepath2 = getFixturePath('file2.yaml');
-  expect(genDiff(filepath1, filepath2)).toBe(result);
+  expect(genDiff(file1, file2)).toBe(expected);
 });
