b5933c81284d80d75c5668d8f4d09c257f0c4f6e
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-03-29 00:31:35 +0300

    update formatters index

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,7 +1,7 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
 
-const formatters = { stylish, plain };
+const formatters = { stylish, plain, json: JSON.stringify };
 
 const format = (diff, formatName) => formatters[formatName](diff);
 
