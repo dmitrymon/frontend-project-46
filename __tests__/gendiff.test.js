25c0cd9fbe7813e19c21a23cf41f0289b20c55b5
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-03-29 00:30:20 +0300

    update __tests__/gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -17,12 +17,15 @@ const yamlFile2 = getFixturePath('file2.yaml');
 const getExpected = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const expectedStylish = getExpected('expectedStylish.txt');
 const expectedPlain = getExpected('expectedPlain.txt');
+const expectedJSON = getExpected('expectedJSON.txt');
 
 test.each([
 { file1: jsonFile1, file2: jsonFile2, formatName: 'stylish', expected: expectedStylish },
 { file1: jsonFile1, file2: jsonFile2, formatName: 'plain', expected: expectedPlain },
+{ file1: jsonFile1, file2: jsonFile2, formatName: 'json', expected: expectedJSON },
 { file1: yamlFile1, file2: yamlFile2, formatName: 'stylish', expected: expectedStylish },
 { file1: yamlFile1, file2: yamlFile2, formatName: 'plain', expected: expectedPlain },
+{ file1: yamlFile1, file2: yamlFile2, formatName: 'json', expected: expectedJSON },
 ])('diff tests', (file1, file2, formatName, expected) => {
 
   expect(genDiff(file1, file2, formatName)).toBe(expected);
